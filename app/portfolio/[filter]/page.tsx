1d3540aef8b13a4a0e81cdc75afc612625631555
Talhary <talhariaz5425869@gmail.com>
2024-08-04 12:06:46 +0500

    Removed Loading

--- app/portfolio/[filter]/page.tsx
+++ app/portfolio/[filter]/page.tsx
@@ -1,12 +1,8 @@
 
 
 const Page =async ({ params: { filter }}: { params: { filter: string }}) => {
-    await new Promise((resolve,rejext)=>{
-        setTimeout(()=>{
-            resolve(null)
-       },2000)
-    } )
-    
+ 
+
     return (
         <div>
           
