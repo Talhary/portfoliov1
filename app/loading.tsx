855c960bab5828b6972cda3d635466b5f3ece97a
Talhary <talhariaz5425869@gmail.com>
2024-08-04 12:10:54 +0500

    Removed Loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,9 @@
-import { Spinner } from "@/components/spinner"
 
 
 const Loading =  ()=>{
     return <div className="flex-col text-center h-[100vh] flex items-center justify-center ">
       
-      <Spinner/>
+       'Loading...'
     </div>
 }
 export default Loading 
--- app/portfolio/[filter]/loading.tsx
+++ app/portfolio/[filter]/loading.tsx
@@ -1,10 +1,10 @@
-import { Spinner } from "@/components/spinner"
+
 
 
 const Loading = ()=>{
     return <div className="flex-col text-center h-[10vh] flex items-center justify-center ">
       
-      <Spinner/>
+      Loading...
     </div>
 }
 export default Loading
