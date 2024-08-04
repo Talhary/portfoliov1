e13f78804ef10ff27544807df17870257dbe2993
Talhary <talhariaz5425869@gmail.com>
2024-08-04 11:58:15 +0500

    Fixed eslint issues

--- app/portfolio/loading.tsx
+++ app/portfolio/loading.tsx
@@ -1,9 +1,10 @@
 import { Spinner } from "@/components/spinner"
 
 
-export default ()=>{
+const Loading =  ()=>{
     return <div className="flex-col text-center h-[10vh] flex items-center justify-center ">
       
       <Spinner/>
     </div>
 }
+export default Loading 
