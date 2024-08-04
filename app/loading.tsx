5460ccf9af12904101d0723f5b2e41feeb36735f
Talhary <talhariaz5425869@gmail.com>
2024-08-04 12:17:44 +0500

    Added Loading text

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@
 const Loading =  ()=>{
     return <div className="flex-col text-center h-[100vh] flex items-center justify-center ">
       
-       'Loading...'
+       Loading...
     </div>
 }
 export default Loading 
