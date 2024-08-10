7052605dbf6901d79a8126bb97088ebfad4dec03
Talhary <talhariaz5425869@gmail.com>
2024-08-10 13:17:52 +0500

    sdfds

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,13 +1,13 @@
-import { cookies } from "next/headers"
-import jwt from 'jsonwebtoken'
-import { redirect } from "next/navigation"
-import {ProfileForm} from './upload-data'
-import Projects from './projects'
-import {GetAllProjects} from '@/actions/getAllProjects';
-import {Heading} from "@/components/heading"
-const Page = async ()=>{
-const res = await GetAllProjects()
+import { cookies } from "next/headers";
+import jwt from 'jsonwebtoken';
+import { redirect } from "next/navigation";
+import { ProfileForm } from './upload-data';
+import Projects from './projects';
+import { GetAllProjects } from '@/actions/getAllProjects';
+import { Heading } from "@/components/heading";
 
+const Page = async()=>{
+const res = await GetAllProjects()
 const token = cookies().get('token')
 if(!token){
     redirect('/admin/login')
@@ -19,15 +19,24 @@ try {
   } catch(err) {
     
     redirect('/admin/login')
+
   }
-  
-return <div>
-    <Heading title="Admin Dashboard" />
-    
-    <ProfileForm/>
-    <Projects initialItems={res}/>
-     
-   
-</div>
-}
-export default Page
+
+  try {
+    jwt.verify(token.value, process.env.JWT_TOKEN as string);
+  } catch (err) {
+    console.error('JWT Verification Failed:', err);
+    redirect('/admin/login');
+    return null; // This line ensures no further execution
+  }
+
+  return (
+    <div>
+      <Heading title="Admin Dashboard" />
+      <ProfileForm />
+      <Projects initialItems={res} />
+    </div>
+  );
+};
+
+export default Page;
