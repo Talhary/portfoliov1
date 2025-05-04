aca474c3be1c6c0f50c6b47498fe99d833a068d6
talhary <talhariaz5425869@gmail.com>
2025-05-05 00:06:24 +0500

    changed

--- app/portfolio/[filter]/page.tsx
+++ app/portfolio/[filter]/page.tsx
@@ -1,17 +1,17 @@
-import {AllProjects} from '@/components/all-projects'
+import { AllProjects } from '@/components/all-projects'
 import { GetAllProjects } from "@/actions/getAllProjects";
-const Page =async ({ params: { filter }}: { params: { filter: string }}) => {
- const res = await GetAllProjects(filter);
- 
-  if(filter == 'all') return <div>
-    <AllProjects projects={res} />
-  </div>
- const data = res.filter((el:any)=>el.type.split('|').indexOf(filter) != -1)
+const Page = async ({ params: { filter } }: { params: { filter: string } }) => {
+    const res = await GetAllProjects(filter);
+
+    if (filter == 'all') return <div>
+        <AllProjects projects={res} />
+    </div>
+    const data = res.filter((el: any) => el.type.split('|').indexOf(filter) != -1)
 
     return (
         <div>
-          
-    <AllProjects projects={data} />
+
+            <AllProjects projects={data} />
         </div>
     );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,10 @@
 
+import { Loader2 } from 'lucide-react';
 import { redirect } from 'next/navigation';
 const Page = async () => {
- redirect('/portfolio/all')
+    redirect('/portfolio/all')
+    return <div>
+        <Loader2 className='animate-spin h-5 w-5 dark:text-white'/>
+    </div>
 };
 export default Page;
