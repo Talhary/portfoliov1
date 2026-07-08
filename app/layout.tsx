72a0b340b4ec19acedcda0a93be0e1e7fd575e30
talhary-dot <talha@woltrio.com>
2026-07-08 11:39:10 +0500

    Update Google AdSense verification meta tag and remove old script

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import Aside from "@/components/aside";
 import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider"
 import { GetToTopButton } from "@/components/get-to-top";
-import AdSense from "@/components/ads";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +16,9 @@ export const metadata: Metadata = {
       "wHIxtQ2Ehw-Z4mysz4nMFthsRdGwaCmbnp72t9r7mkI",
       "6T7Bjp5t8IbCIRz0m8tBXAwsUrieJPo__M6ZGbdPTLA"
     ],
+  },
+  other: {
+    "google-adsense-account": "ca-pub-5705856509590868",
   },
 };
 
@@ -41,7 +43,6 @@ export default async function RootLayout({
 
           </aside>
           <div className='flex-auto max-md:ml-0 relative min-w-0 max-w-full overflow-hidden'>
-            <AdSense />
             <Navbar className='z-50' />
             <div className="bg-zinc-50 dark:bg-big-card border border-zinc-200 dark:border-none shadow-sm dark:shadow-black relative ml-0 text-zinc-900 dark:text-zinc-100 max-md:m-4 max-xs:m-1 max-xs:p-3 rounded-2xl p-5 max-md:p-3 w-full max-w-full overflow-hidden">
               <GetToTopButton />
